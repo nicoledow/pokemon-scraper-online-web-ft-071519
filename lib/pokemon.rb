cd0e15304f2b2b81efb07b339c56ca394188cc95
Nicole Dow <nikki.dow71@gmail.com>
2019-08-14 23:33:17 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+          INSERT INTO pokemon (name, type)
+          VALUES (?, ?)
+          SQL
+    db.execute(sql, name, type)
+  end
+
+  def self.find(id, db)
+    sql = "SELECT * FROM pokemon WHERE pokemon.id = id"
+    db.execute(sql, id).map {|row| self.new(row)}
+    binding.pry
+  end
+
+
+
 end
